a376a9f8532c06494dbae1c2ba8000fda0d681e2
Fatima Zahrae EL HAJOUI <113965747+fatimaelhajoui@users.noreply.github.com>
2026-03-11 16:28:38 +0000

    Add files via upload

---
+++ sales/sales/src/main/java/net/elhajoui/sales/services/UserDetailsServiceImpl.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.elhajoui.sales.services;
+
+import java.util.Optional;
+import net.elhajoui.sales.entities.AppUser;
+import net.elhajoui.sales.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author marwa
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+    @Autowired
+    UserRepository userRepository;
+            
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<AppUser> account= userRepository.findOneByUsername(username);
+        
+        if(account.isEmpty()){
+            throw new UsernameNotFoundException("User not found: "+username);
+        }
+        
+        AppUser appUser = account.get();
+        return org.springframework.security.core.userdetails.User
+            .withUsername(appUser.getUsername())
+            .password(appUser.getPassword())
+            .authorities(appUser.getRole())
+            .build();
+
+    }
+    
+}
