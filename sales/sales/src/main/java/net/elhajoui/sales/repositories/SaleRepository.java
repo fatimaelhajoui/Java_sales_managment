cbf74ce1f67dafc34f3bf0cbade39ee1d46e9b52
Fatima Zahrae EL HAJOUI <113965747+fatimaelhajoui@users.noreply.github.com>
2026-03-30 18:07:17 +0100

    Add files via upload

---
+++ sales/sales/src/main/java/net/elhajoui/sales/repositories/SaleRepository.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.elhajoui.sales.repositories;
+
+import java.util.List;
+import net.elhajoui.sales.entities.AppUser;
+import net.elhajoui.sales.entities.Sale;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author marwa
+ */
+public interface SaleRepository extends JpaRepository<Sale ,Long>{
+    List<Sale> findByAgentUsername(String username);
+    Page<Sale> findByAgent_IdAndContractIdContaining(Long user_id, String keyword, Pageable page);
+    
+    
+}
