f25950d42c465c7579c31905ebd6babdb03c43f4
Fatima Zahrae EL HAJOUI <113965747+fatimaelhajoui@users.noreply.github.com>
2026-04-03 17:03:25 +0100

    Refactor SaleRepository with enhanced comments
    Added comments to clarify method purposes.

--- sales/sales/src/main/java/net/elhajoui/sales/repositories/SaleRepository.java
+++ sales/sales/src/main/java/net/elhajoui/sales/repositories/SaleRepository.java
@@ -16,15 +16,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author marwa
  */
 public interface SaleRepository extends JpaRepository<Sale ,Long>{
-     //agent
+    //agent
     List<Sale> findByAgentUsername(String username);
-    Page<Sale> findByAgent_IdAndContractIdContaining(Long user_id, String keyword, Pageable page);
-    Sale findByAgent_IdAndId(Long user_id, Long sale_id);
+    Page<Sale> findByAgent_IdAndContractIdContaining(Long user_id, String keyword, Pageable page); //list sales
+    Sale findByAgent_IdAndId(Long user_id, Long sale_id); //find sale 
     
     
     //manager
-    Page<Sale> findByAgent_Team_IdAndContractIdContaining(Long teamId, String keyword, Pageable page);
-    Sale findByAgent_Team_IdAndId(Long user_id, Long sale_id);
+    Page<Sale> findByAgent_Team_IdAndContractIdContaining(Long teamId, String keyword, Pageable page); //list sales in the same team as the manager
+    Sale findByAgent_Team_IdAndId(Long user_id, Long sale_id); //find sale 
+    
 
     //admin
     Page<Sale> findByContractIdContaining(String keyword, Pageable page);
