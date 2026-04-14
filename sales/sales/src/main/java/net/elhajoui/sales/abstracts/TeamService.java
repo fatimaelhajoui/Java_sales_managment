e1427fe517d0b14a3044cb8fcea565bbd97d6ca9
Fatima Zahrae EL HAJOUI <113965747+fatimaelhajoui@users.noreply.github.com>
2026-04-14 17:35:44 +0100

    Remove license comments from TeamService.java

--- sales/sales/src/main/java/net/elhajoui/sales/abstracts/TeamService.java
+++ sales/sales/src/main/java/net/elhajoui/sales/abstracts/TeamService.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package net.elhajoui.sales.abstracts;
 
 import java.util.List;
 import net.elhajoui.sales.entities.Team;
 import org.springframework.data.domain.Page;
 
-/**
- *
- * @author marwa
- */
 
 public interface TeamService {
     Team OneTeam(Long user_id);
