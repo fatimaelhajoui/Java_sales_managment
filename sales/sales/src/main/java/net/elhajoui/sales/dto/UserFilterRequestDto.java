3a7b52ae110fc50284f37d1b5df93f1e0fdce335
Fatima Zahrae EL HAJOUI <113965747+fatimaelhajoui@users.noreply.github.com>
2026-04-21 12:16:47 +0100

    Remove unnecessary comments from UserFilterRequestDto

--- sales/sales/src/main/java/net/elhajoui/sales/dto/UserFilterRequestDto.java
+++ sales/sales/src/main/java/net/elhajoui/sales/dto/UserFilterRequestDto.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package net.elhajoui.sales.dto;
 
 import lombok.*;
 
-/**
- *
- * @author marwa
- */
+
 @Getter @Setter
 public class UserFilterRequestDto {
     private String keyword="";
