3e7ff73a0697a11a9839e3e7849fb60f812762e1
Fatima Zahrae EL HAJOUI <113965747+fatimaelhajoui@users.noreply.github.com>
2026-04-21 12:15:13 +0100

    Remove unnecessary comments and clean up TeamController

--- sales/sales/src/main/java/net/elhajoui/sales/controllers/TeamController.java
+++ sales/sales/src/main/java/net/elhajoui/sales/controllers/TeamController.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package net.elhajoui.sales.controllers;
 
 import jakarta.validation.Valid;
@@ -16,10 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- *
- * @author marwa
- */
 @Controller
 public class TeamController {
     @Autowired
