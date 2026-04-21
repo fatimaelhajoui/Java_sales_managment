e70b1a00472582fee4a3dc671b49fcd07d0a87f4
Fatima Zahrae EL HAJOUI <113965747+fatimaelhajoui@users.noreply.github.com>
2026-04-21 12:14:11 +0100

    Refactor SecurityConfig by removing comments

--- sales/sales/src/main/java/net/elhajoui/sales/config/SecurityConfig.java
+++ sales/sales/src/main/java/net/elhajoui/sales/config/SecurityConfig.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package net.elhajoui.sales.config;
 
 import lombok.RequiredArgsConstructor;
@@ -17,10 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-/**
- *
- * @author marwa
- */
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
