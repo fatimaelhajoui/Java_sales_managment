57ff1e68aa86cd7dd8c8588a615bb2479e463931
Fatima Zahrae EL HAJOUI <113965747+fatimaelhajoui@users.noreply.github.com>
2026-04-21 12:14:35 +0100

    Clean up AuthController.java by removing comments
    Removed unnecessary comments and license information.

--- sales/sales/src/main/java/net/elhajoui/sales/controllers/AuthController.java
+++ sales/sales/src/main/java/net/elhajoui/sales/controllers/AuthController.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package net.elhajoui.sales.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- *
- * @author marwa
- */
 @Controller 
 public class AuthController {
     @GetMapping("/login")
