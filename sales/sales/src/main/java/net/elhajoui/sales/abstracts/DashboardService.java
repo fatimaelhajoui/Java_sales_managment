7147f83b7cc897647ca9da30a3353010f642dc1b
Fatima Zahrae EL HAJOUI <113965747+fatimaelhajoui@users.noreply.github.com>
2026-04-24 17:24:50 +0100

    Add user_id parameter to getTeamRankingCurrentMonth

--- sales/sales/src/main/java/net/elhajoui/sales/abstracts/DashboardService.java
+++ sales/sales/src/main/java/net/elhajoui/sales/abstracts/DashboardService.java
@@ -1,23 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package net.elhajoui.sales.abstracts;
 
+import java.util.List;
 import net.elhajoui.sales.dto.SaleFilterRequestDto;
 import net.elhajoui.sales.dto.*;
 
-/**
- *
- * @author marwa
- */
 public interface DashboardService {
     
     //counts for total / approved / rejected / pending
     SaleSummaryDto getSalesSammury(Long user_id);
     
     //teams sorted by sales count
-    TeamsClassmentDto getTeamRankingCurrentMonth();
+   List<TeamsClassmentDto> getTeamRankingCurrentMonth(Long user_id);
     
     
 }
