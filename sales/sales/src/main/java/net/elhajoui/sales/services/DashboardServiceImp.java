e86391dca184e6c14a44731b1f3fb71f0c0eb24f
Fatima Zahrae EL HAJOUI <113965747+fatimaelhajoui@users.noreply.github.com>
2026-04-25 12:49:26 +0100

    Enhance DashboardServiceImp with team ranking method
    Added team ranking functionality and improved structure.

--- sales/sales/src/main/java/net/elhajoui/sales/services/DashboardServiceImp.java
+++ sales/sales/src/main/java/net/elhajoui/sales/services/DashboardServiceImp.java
@@ -1,23 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package net.elhajoui.sales.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import net.elhajoui.sales.abstracts.DashboardService;
 import net.elhajoui.sales.dto.SaleSummaryDto;
+import net.elhajoui.sales.dto.TeamsClassmentDto;
 import net.elhajoui.sales.entities.AppUser;
+import net.elhajoui.sales.entities.Sale;
+import net.elhajoui.sales.entities.Team;
 import net.elhajoui.sales.repositories.SaleRepository;
+import net.elhajoui.sales.repositories.TeamRepository;
 import net.elhajoui.sales.repositories.UserRepository;
 import net.elhajoui.sales.specification.SaleSpecification;
 import static net.elhajoui.sales.specification.SaleSpecification.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.PredicateSpecification;
 import org.springframework.stereotype.Service;
 
-/**
- *
- * @author marwa
- */
+
 
 @Service
 public class DashboardServiceImp implements DashboardService{
@@ -29,6 +30,8 @@ public class DashboardServiceImp implements DashboardService{
     @Autowired
     UserRepository userRepository;
     
+    @Autowired
+    TeamRepository teamRepository;
 
     @Override
     public SaleSummaryDto getSalesSammury(Long user_id) {
@@ -60,6 +63,39 @@ public class DashboardServiceImp implements DashboardService{
         
         return summary;
         
+        
+    }
+
+    @Override
+    public List<TeamsClassmentDto> getTeamRankingCurrentMonth(Long user_id) {
+        
+        AppUser loggedInUser = userRepository.findById(user_id)
+        .orElseThrow(() -> new RuntimeException("User not found"));
+        
+        
+        
+        List<Team> teams = teamRepository.findAll();
+        List<TeamsClassmentDto> teamsSales = new ArrayList<>();
+        
+        if(("ADMIN").equalsIgnoreCase(loggedInUser.getRole())){
+        
+        for(Team team: teams){
+            int count = (int) saleRepository.count(SaleSpecification.hasTeam(team.getId())
+                        .and(hasCurrentMonth()));
+            
+            TeamsClassmentDto classmentDto = new TeamsClassmentDto();
+            classmentDto.setSalesCount(count);
+            classmentDto.setTeam(team.getName());
+            
+            teamsSales.add(classmentDto);
+        }
+        
+        teamsSales.sort((a, b) -> b.getSalesCount() - a.getSalesCount());
+        
+        
+        
+        }
+        return teamsSales; 
         
     }
 
