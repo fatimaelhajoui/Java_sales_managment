f5933b7865e97e3560165438ce50acee3102b1b1
Fatima Zahrae EL HAJOUI <113965747+fatimaelhajoui@users.noreply.github.com>
2026-04-25 12:41:47 +0100

    Remove commented license and author information

--- sales/sales/src/main/java/net/elhajoui/sales/dto/TeamsClassmentDto.java
+++ sales/sales/src/main/java/net/elhajoui/sales/dto/TeamsClassmentDto.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package net.elhajoui.sales.dto;
 
 import lombok.Getter;
 import lombok.Setter;
 
-/**
- *
- * @author marwa
- */
 @Getter @Setter
 public class TeamsClassmentDto {
     private String team;
