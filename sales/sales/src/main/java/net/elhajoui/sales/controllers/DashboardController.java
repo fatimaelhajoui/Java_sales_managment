7de9c2445a874a3c91bd42d937226b6ac3d6718f
Fatima Zahrae EL HAJOUI <113965747+fatimaelhajoui@users.noreply.github.com>
2026-05-07 16:59:09 +0100

    Enhance DashboardController with recent sales and agents
    Added functionality to retrieve and display recent sales, top agents, and monthly sales data in the dashboard.

--- sales/sales/src/main/java/net/elhajoui/sales/controllers/DashboardController.java
+++ sales/sales/src/main/java/net/elhajoui/sales/controllers/DashboardController.java
@@ -1,9 +1,17 @@
 package net.elhajoui.sales.controllers;
 
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import net.elhajoui.sales.dto.AgentsClassmentDto;
+import net.elhajoui.sales.dto.RecentSalesDto;
 import net.elhajoui.sales.dto.SaleSummaryDto;
+import net.elhajoui.sales.dto.TeamsClassmentDto;
+import net.elhajoui.sales.entities.Sale;
 import net.elhajoui.sales.services.CustomUserDetails;
 import net.elhajoui.sales.services.DashboardServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,9 +26,9 @@ public class DashboardController {
     private DashboardServiceImp dashboardServiceImp;
 
     @GetMapping("/dashboard")
-    public String dashboard(Model model){
+    public String dashboard(Model model, RecentSalesDto filter){
         
-       CustomUserDetails loggedInUser = (CustomUserDetails) SecurityContextHolder
+        CustomUserDetails loggedInUser = (CustomUserDetails) SecurityContextHolder
         .getContext()
         .getAuthentication()
         .getPrincipal();
@@ -31,10 +39,37 @@ public class DashboardController {
         
         List<TeamsClassmentDto> TopTeams = dashboardServiceImp.getTeamRankingCurrentMonth(userId);
         
+        List<AgentsClassmentDto> TopAgents= dashboardServiceImp.getAgentRankingCurrentMonth(userId);
+        
+        Page<Sale> RecentSales= dashboardServiceImp.getRecentSales(userId, filter);
+        
+        List<Integer> monthlySales = dashboardServiceImp.getMonthlySalesCurrentYear(userId);
+
+        
         model.addAttribute( "Summary", SaleSUM );
         model.addAttribute("TopTeams", TopTeams);
+        model.addAttribute("TopAgents", TopAgents);
+        model.addAttribute("RecentSales", RecentSales.getContent());
+        model.addAttribute("totalPages", new int[RecentSales.getTotalPages()]);
+        model.addAttribute("currentPage", filter.getPage());
+        
+        //chart
+        model.addAttribute("monthlySales", monthlySales);
+
+        
+        //to hide section by role of user
+        model.addAttribute("userRole", loggedInUser.getAuthorities().iterator().next().getAuthority());
         
         return "dashboard/dash";
-        
+    }
+    
+    
+    @GetMapping("/dashboard/export")
+    public void exportRecentSales(HttpServletResponse response, RecentSalesDto filter) throws IOException {
+
+    CustomUserDetails loggedInUser = (CustomUserDetails) SecurityContextHolder
+        .getContext().getAuthentication().getPrincipal();
+
+    dashboardServiceImp.exportRecentSalesToExcel(loggedInUser.getId(), filter, response);
     }
 }
