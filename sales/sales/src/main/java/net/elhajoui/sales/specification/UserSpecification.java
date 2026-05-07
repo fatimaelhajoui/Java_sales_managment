b45f1f4b75c6559f51f726847444ecbfdf92257d
Fatima Zahrae EL HAJOUI <113965747+fatimaelhajoui@users.noreply.github.com>
2026-05-07 17:16:26 +0100

    Remove unnecessary comments and clean up UserSpecification

--- sales/sales/src/main/java/net/elhajoui/sales/specification/UserSpecification.java
+++ sales/sales/src/main/java/net/elhajoui/sales/specification/UserSpecification.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package net.elhajoui.sales.specification;
 
 import net.elhajoui.sales.entities.AppUser;
 import org.springframework.data.jpa.domain.Specification;
 
-/**
- *
- * @author marwa
- */
+
 public class UserSpecification {
      // this for status
     public static Specification<AppUser> hasStatus(Boolean status) {
