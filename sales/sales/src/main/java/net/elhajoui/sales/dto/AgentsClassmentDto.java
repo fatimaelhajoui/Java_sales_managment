a532c659f327ec65f0cf0a3279058d609283a1bd
Fatima Zahrae EL HAJOUI <113965747+fatimaelhajoui@users.noreply.github.com>
2026-05-07 17:08:36 +0100

    Add files via upload

---
+++ sales/sales/src/main/java/net/elhajoui/sales/dto/AgentsClassmentDto.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.elhajoui.sales.dto;
+
+import lombok.*;
+
+/**
+ *
+ * @author marwa
+ */
+@Getter @Setter
+public class AgentsClassmentDto {
+    private String agent;
+    private String team;
+    private int salesCount;  
+}
---
+++ sales/sales/src/main/java/net/elhajoui/sales/dto/RecentSalesDto.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.elhajoui.sales.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author marwa
+ */
+@Getter @Setter
+
+public class RecentSalesDto {
+     private int page = 0;
+    private int size = 5;
+}
